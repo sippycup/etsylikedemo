051019121c04ab76a515ff638b74fe3e65c3abc9
Joseph Smith <jcsmith1234@gmail.com>
2015-09-07 00:45:10 -0400

    Assocate listings with users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150907024814) do
+ActiveRecord::Schema.define(version: 20150907042459) do
 
   create_table "listings", force: :cascade do |t|
     t.string   "name"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20150907024814) do
     t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
